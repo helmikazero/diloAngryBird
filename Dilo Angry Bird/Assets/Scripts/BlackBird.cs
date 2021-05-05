3c815e41daa5da8775591df14e7a341705b9708d
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2021-05-05 12:47:41 +0700

    FINISH

---
+++ Dilo Angry Bird/Assets/Scripts/BlackBird.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBird : Bird
+{
+    public GameObject explosion;
+
+    void Update()
+    {
+        if (State == BirdState.HitSomething)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+    }
+}
